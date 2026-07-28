3775f0035f48911ce2a48a4ca18684bdf5d3cf51
juddrussel <judacutey@gmail.com>
2026-07-28 23:27:50 +0800

    fix for login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -28,7 +28,7 @@ export default function Login() {
     setLoading(true);
     setAlertMsg(null);
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
